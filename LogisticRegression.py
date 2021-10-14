e78c1367fe26e2af609b5f287ac50cd6420d59d3
AliTosJM <alitosjm@gmail.com>
2021-10-13 19:28:13 -0500

    drop na values

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
 LoanData = pd.read_csv("./data/01Exercise1.csv")
+
+LoanPrep = LoanData.copy()
+
+# Idenyify missigin values
+print(LoanPrep.isnull().sum(axis=0))
+
+# drop rows with missing values
+LoanPrep = LoanPrep.dropna()
