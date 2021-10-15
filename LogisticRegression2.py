b3b69f36fd7449acf0701e885c73bfb76d44a288
AliTosJM <alitosjm@gmail.com>
2021-10-15 03:07:45 -0500

    check LR2 up

--- LogisticRegression2.py
+++ LogisticRegression2.py
@@ -42,6 +42,7 @@ y_pred = classifier.predict(X_test)
 # Elaborar una matriz de confusión
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print("cm:", cm, sep="\n")
 
 # Representación gráfica de los resultados del algoritmo en el Conjunto de Entrenamiento
 from matplotlib.colors import ListedColormap
@@ -51,7 +52,8 @@ plt.figure(1)
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+probs = classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape)
+plt.contourf(X1, X2, probs,
              alpha=0.75, cmap=ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
@@ -82,3 +84,5 @@ plt.xlabel('Edad')
 plt.ylabel('Sueldo Estimado')
 plt.legend()
 plt.show()
+
+print("one point", classifier.predict([[-0.1, 1.24]]))
