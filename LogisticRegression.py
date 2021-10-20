55f26b9a5a4f7f72de9c35a366e001176e195175
AliTosJM <alitosjm@gmail.com>
2021-10-19 20:14:08 -0500

    better aproach

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -62,9 +62,15 @@ if categories.eq(True).any():
 from sklearn.preprocessing import StandardScaler
 scalar_ = StandardScaler()
 
-LoanPrep['income'] = scalar_.fit_transform(LoanPrep[['income']])
-# LoanPrep['loanamt'] = scalar_.fit_transform(LoanPrep[['loanamt']])
-LoanPrep['loanamt'] = scalar_.transform(LoanPrep[['loanamt']])
+for label, value in NumericOneZero.items():
+    if not value:
+        print(f"Index : {label}, Value : {value}")
+        LoanPrep[label] = scalar_.fit_transform(LoanPrep[[label]])
+
+
+# LoanPrep['income'] = scalar_.fit_transform(LoanPrep[['income']])
+# x LoanPrep['loanamt'] = scalar_.fit_transform(LoanPrep[['loanamt']])
+# LoanPrep['loanamt'] = scalar_.transform(LoanPrep[['loanamt']])
 
 
 # Create the X (Independent) and Y (Dependent) dataframes
@@ -81,7 +87,7 @@ Y_ = LoanPrep.iloc[:, 4].values
 
 # Split the X and Y dataset into training and testing set
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X_, Y_, test_size=0.3, random_state=1234, stratify=Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X_, Y_, test_size=0.3, random_state=1234, stratify=Y_)
 # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
 # Build the Logistic Regression model
